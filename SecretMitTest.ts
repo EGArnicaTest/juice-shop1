66d644c4adf8d34cfd9ab7971c20d6bb649f7121
Ethan Gaddis <ethan@arnica.io>
2025-06-13 16:37:13 -0400

    removed JWS tokens from SecretMitTest & SecretMitTest2

--- SecretMitTest.ts
+++ SecretMitTest.ts
@@ -52,19 +52,19 @@ describe('LastLoginIpComponent', () => {
 
   it('should log JWT parsing error to console', () => {
     console.log = jasmine.createSpy('log')
-    localStorage.setItem('token', 'definitelyInvalidJWT')
+    localStorage.setItem()
     component.ngOnInit()
     expect(console.log).toHaveBeenCalled()
   })
 
   xit('should set Last-Login IP from JWT as trusted HTML', () => { // FIXME Expected state seems to leak over from previous test case occasionally
-    localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Imxhc3RMb2dpbklwIjoiMS4yLjMuNCJ9fQ.RAkmdqwNypuOxv3SDjPO4xMKvd1CddKvDFYDBfUt3bg')
+    localStorage.setItem()
     component.ngOnInit()
     expect(sanitizer.bypassSecurityTrustHtml).toHaveBeenCalledWith('<small>1.2.3.4</small>')
   })
 
   xit('should not set Last-Login IP if none is present in JWT', () => { // FIXME Expected state seems to leak over from previous test case occasionally
-    localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7fX0.bVBhvll6IaeR3aUdoOeyR8YZe2S2DfhGAxTGfd9enLw')
+    localStorage.setItem()
     component.ngOnInit()
     expect(sanitizer.bypassSecurityTrustHtml).not.toHaveBeenCalled()
   })
