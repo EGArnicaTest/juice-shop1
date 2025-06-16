c4e49bf429825147c79946d244534c255e23a0e6
Ethan Gaddis <ethan@arnica.io>
2025-06-16 15:10:44 -0400

    deleted JWSTokenTest file

--- JWSTokenTest.ts
+++
@@ -1,7 +0,0 @@
-// Simulated JWT token for testing secret scanning tools
-const fakeJwtToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
-  "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkV0aGFuIEdhZGRpcyIsImlhdCI6MTUxNjIzOTAyMn0." +
-  "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
-
-console.log("Simulated JWT Token:", fakeJwtToken);
